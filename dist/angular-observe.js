b4fd29df5137a52033d1544179e272ad8da13635
Geoffrey Goodman <ggoodman@gmail.com>
2015-12-02 16:57:06 -0500

    v2.0.0

--- dist/angular-observe.js
+++ dist/angular-observe.js
@@ -60,38 +60,30 @@ return /******/ (function(modules) { // webpackBootstrap
 
 	var mod = Angular.module(module.exports, []);
 
-	mod.directive('observe', ['$compile', '$timeout', function ($compile, $timeout) {
+	mod.directive('asyncBind', ['$compile', '$q', '$rootScope', '$timeout', function ($compile, $q, $rootScope, $timeout) {
 	    return {
 	        restrict: 'EA',
 	        scope: true,
-	        transclude: {
-	            loading: '?loading',
-	            active: '?active',
-	            complete: '?complete',
-	            error: '?error',
-	        },
-	        link: function ($scope, $element, $attrs, ctl, $transclude) {
+	        compile: compile,
+	    };
+	    
+	    function compile(tElement) {
+	        var stateLinkFunctions = getStateLinkFunctions(tElement);
+	        
+	        return function postLink($scope, $element, $attrs) {
+	            var childScope;
 	            var currentState;
-	            var stateLinkFunctions = {};
+	            var isolateScope = $rootScope.$new(true, $scope);
 	            var source = $scope.$eval($attrs.observe || $attrs.source);
+	            
+	            $element.empty();
 	            
 	            if (!source) {
 	                console.warn('The `observable` directive requires a source observable.');
 	                return;
 	            }
 	            
-	            $transclude(compileState.bind(null, 'active', false), null, '');
-	            $transclude(compileState.bind(null, 'loading', true), null, 'loading');
-	            $transclude(compileState.bind(null, 'active', true), null, 'active');
-	            $transclude(compileState.bind(null, 'complete', true), null, 'complete');
-	            $transclude(compileState.bind(null, 'error', true), null, 'error');
-	            
-	            if (!stateLinkFunctions.active) {
-	                console.warn('The `observable` directive requires at least one child element.');
-	                return;
-	            }
-	            
-	            setState('loading', true);
+	            setState('loading');
 	            
 	            // Subscribe to the observable
 	            var observable = typeof source.subscribe === 'function'
@@ -100,16 +92,27 @@ return /******/ (function(modules) { // webpackBootstrap
 	                    ?   liftPromise(source)
 	                    :   liftValue(source);
 	            var subscription = observable.subscribe(onNext, onError, onComplete);
-
+	    
 	            // Unsubscribe when this element is destroyed
 	            $scope.$on('$destroy', function() {
 	                subscription.unsubscribe();
 	            });
 	            
 	            function onNext(val) {
-	                $scope.$value = val;
-
-	                setState('active');
+	                var forceLink = !isPrimitive(isolateScope.$value)
+	                    || !isPrimitive(val);
+	                
+	                isolateScope.$value = val;
+	    
+	                setState('active', forceLink);
+	                
+	                function isPrimitive(object) {
+	                    var type = typeof object;
+	                    
+	                    return type === 'boolean'
+	                        || type === 'number'
+	                        || type === 'string';
+	                }
 	            }
 	            
 	            function onComplete(val) {
@@ -117,75 +120,119 @@ return /******/ (function(modules) { // webpackBootstrap
 	            }
 	            
 	            function onError(error) {
-	                $scope.$error = error;
+	                isolateScope.$error = error;
+	                
 	                setState('error');
 	            }
 	            
-	            function compileState(state, useContents, clone) {
-	                var contents = useContents
-	                    ?   clone.contents()
-	                    :   clone;
-	                
-	                if (contents.length) {
-	                    var linkFunction = $compile(contents);
-	                
-	                    stateLinkFunctions[state] = linkFunction;
-	                }
-	            }
-	            
-	            function liftPromise(source) {
-	                return {
-	                    subscribe: function (onNext, onError, onComplete) {
-	                        source.then(onNext, onError, onNext)
-	                            .catch(function (reason) {
-	                                return reason;
-	                            })
-	                            .then(onComplete);
-	                        
-	                        return {
-	                            unsubscribe: Angular.noop,
-	                        };
-	                    }
-	                };
-	            }
-	            
-	            function liftValue(source) {
-	                return {
-	                    subscribe: function (onNext, onError, onComplete) {
-	                        $timeout(onNext.bind(null, source), 0, false)
-	                            .then(onComplete);
-	                        
-	                        return {
-	                            unsubscribe: Angular.noop,
-	                        };
-	                    }
-	                };
-	            }
-	            
-	            function setState(state, skipDigest) {
-	                if (state !== currentState) {
+	            function setState(state, forceLink) {
+	                if (forceLink || state !== currentState) {
 	                    var linkFunction = stateLinkFunctions[state];
 	                    
-	                    $element.empty();
+	                    if (childScope) {
+	                        childScope.$destroy();
+	                    }
+	                    
 	                    
 	                    if (!linkFunction) {
+	                        $element.empty();
 	                        return;
 	                    }
 	                    
-	                    var replacement = linkFunction($scope);
-	                    
-	                    $element.append(replacement);
-	                    
+	                    childScope = isolateScope.$new();
 	                    currentState = state;
+	                    
+	                    linkFunction(childScope, function (clone) {
+	                        $element.empty();
+	                        $element.append(clone);
+	                    });
 	                }
 	                
-	                if (!skipDigest && !$scope.$root.$$phase) {
-	                    $scope.$digest(true);
+	                if (!isolateScope.$root.$$phase) {
+	                    isolateScope.$digest(true);
 	                }
 	            }
+	        };
+	    }
+	    
+	    function liftPromise(source) {
+	        return {
+	            subscribe: function (onNext, onError, onComplete) {
+	                source.then(onNext, onError, onNext)
+	                    .catch(function (reason) {
+	                        return reason;
+	                    })
+	                    .then(onComplete);
+	                
+	                return {
+	                    unsubscribe: Angular.noop,
+	                };
+	            }
+	        };
+	    }
+	    
+	    function liftValue(source) {
+	        return {
+	            subscribe: function (onNext, onError, onComplete) {
+	                $timeout(onNext.bind(null, source), 0, false)
+	                    .then(onComplete);
+	                
+	                return {
+	                    unsubscribe: Angular.noop,
+	                };
+	            }
+	        };
+	    }
+
+
+	    function getStateLinkFunctions(tElement) {
+	        var stateLinkFunctions = {};
+	        var stateTemplates = {
+	            loading: [],
+	            active: [],
+	            error: [],
+	            complete: [],
+	        };
+	        var template = [];
+	        
+	        Angular.forEach(tElement.contents(), function (node) {
+	            switch (node.nodeName.toLowerCase()) {
+	                case 'loading': return stateTemplates.loading.push(node);
+	                case 'active': return stateTemplates.active.push(node);
+	                case 'error': return stateTemplates.error.push(node);
+	                case 'complete': return stateTemplates.complete.push(node);
+	                default: return template.push(node);
+	            }
+	        });
+	        
+	        var hasStateTemplates = false;
+	        
+	        Angular.forEach(stateTemplates, function (template, state) {
+	            if (template.length) {
+	                hasStateTemplates = true;
+	                
+	                var clone = Angular.element(template).clone();
+	                
+	                stateLinkFunctions[state] = $compile(clone);
+	            }
+	        });
+	        
+	        if (!hasStateTemplates) {
+	            if (!template.length) {
+	                template.push(document.createTextNode('{{$value}}'));
+	            }
+	                
+	            var clone = Angular.element(template).clone();
+	            
+	            stateLinkFunctions.active = $compile(clone);
+	            stateLinkFunctions.complete = $compile(clone.clone());
 	        }
-	    };
+	        
+	        return stateLinkFunctions;
+	    }
+
 	}]);
+
 
 /***/ },
 /* 1 */
