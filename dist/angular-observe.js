9c62e56c53f324f04587aaaf40ffdac61a4f0206
Geoffrey Goodman <ggoodman@gmail.com>
2015-12-01 21:44:02 -0500

    v1.1.0

--- dist/angular-observe.js
+++ dist/angular-observe.js
@@ -60,7 +60,7 @@ return /******/ (function(modules) { // webpackBootstrap
 
 	var mod = Angular.module(module.exports, []);
 
-	mod.directive('observe', ['$compile', function ($compile) {
+	mod.directive('observe', ['$compile', '$timeout', function ($compile, $timeout) {
 	    return {
 	        restrict: 'EA',
 	        scope: true,
@@ -71,10 +71,21 @@ return /******/ (function(modules) { // webpackBootstrap
 	            error: '?error',
 	        },
 	        link: function ($scope, $element, $attrs, ctl, $transclude) {
-	            // Subscribe to the observable
-	            var stateLinkFunctions = {};
 	            var currentState;
-	            var observable = $scope.$eval($attrs.observe || $attrs.source);
+	            var stateLinkFunctions = {};
+	            var source = $scope.$eval($attrs.observe || $attrs.source);
+	            
+	            if (!source) {
+	                console.warn('The `observable` directive requires a source observable.');
+	                return;
+	            }
+	            
+	            // Subscribe to the observable
+	            var observable = typeof source.subscribe === 'function'
+	                ?   source
+	                :   typeof source.then === 'function'
+	                    ?   liftPromise(source)
+	                    :   liftValue(source);
 	            var subscription = observable.subscribe(onNext, onError, onComplete);
 	            
 	            $transclude(compileState.bind(null, 'active', false), null, '');
@@ -121,10 +132,37 @@ return /******/ (function(modules) { // webpackBootstrap
 	                }
 	            }
 	            
+	            function liftPromise(source) {
+	                return {
+	                    subscribe: function (onNext, onError, onComplete) {
+	                        source.then(onNext, onError, onNext)
+	                            .catch(function (reason) {
+	                                return reason;
+	                            })
+	                            .then(onComplete);
+	                        
+	                        return {
+	                            unsubscribe: Angular.noop,
+	                        };
+	                    }
+	                };
+	            }
+	            
+	            function liftValue(source) {
+	                return {
+	                    subscribe: function (onNext, onError, onComplete) {
+	                        $timeout(onNext.bind(null, source), 0, false)
+	                            .then(onComplete);
+	                        
+	                        return {
+	                            unsubscribe: Angular.noop,
+	                        };
+	                    }
+	                };
+	            }
+	            
 	            function setState(state, skipDigest) {
 	                if (state !== currentState) {
-	                    $element.empty();
-	                    
 	                    var linkFunction = stateLinkFunctions[state];
 	                    
 	                    if (!linkFunction) {
@@ -133,12 +171,13 @@ return /******/ (function(modules) { // webpackBootstrap
 	                    
 	                    var replacement = linkFunction($scope);
 	                    
+	                    $element.empty();
 	                    $element.append(replacement);
 	                    
 	                    currentState = state;
 	                }
 	                
-	                if (!skipDigest) {
+	                if (!skipDigest && !$scope.$root.$$phase) {
 	                    $scope.$digest(true);
 	                }
 	            }
